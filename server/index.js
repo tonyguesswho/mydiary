183ae5517ec09c576265e37430958bc6a5bfcd04
Anthony <anthonyu234@gmail.com>
2018-07-15 17:25:59 +0100

    Setup babel build folder

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
+
+const port = process.env.port || 3000;
+
 app.get('/', (req, res) => {
   res.send('welcome');
+});
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
 });
