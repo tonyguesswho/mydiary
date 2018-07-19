1ecbe338967f495eb31fc387021480895a08f7cd
Anthony <anthonyu234@gmail.com>
2018-07-19 02:32:13 +0100

    Setup test cases

--- server/index.js
+++ server/index.js
@@ -3,13 +3,16 @@ import entries from '../routes/entries';
 import home from '../routes/home';
 
 const app = express();
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1/entries', entries);
 app.use('/', home);
 
 const port = process.env.port || 3000;
-
 app.listen(port, () => {
   // console.log(`app listening on port ${port}`);
 });
+
+module.exports = { app };
---
+++ test/server.test.js
@@ -0,0 +1,24 @@
+// // const chai = require('chai');
+
+// const expect = require('expect');
+
+// // const chaiHttp = require('chai-http');
+
+// const request = require('supertest');
+
+// const { app } = require('./../build/bundle');
+
+// const entries = require('../data/data');
+
+// // chai.use(chaiHttp);
+
+// describe('GET all entries', () => {
+//   it('should get all entries', () => {
+//     request('https://localhost:3000')
+//       .get('/api/v1/entries')
+//       .expect(200)
+//       .expect(res => {
+//         expect(res.body).toBe(true);
+//       });
+//   });
+// });
