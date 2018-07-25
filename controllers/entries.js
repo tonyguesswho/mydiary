ad01ef845d2f8c0c7c560bf4d6f0b93446ffb57e
Anthony ugwu <anthonyu234@gmail.com>
2018-07-24 22:36:01 -0700

    Use controllers

--- controllers/entries.js
+++ controllers/entries.js
@@ -1,36 +1,34 @@
-
-import entries from '../data/data';
 import joi from 'joi';
 
+import entries from '../data/data';
+
 function validateEntry(body) {
-    const schema = {
-      title: joi
-        .string()
-        .min(1)
-        .required(),
-      description: joi
-        .string()
-        .min(1)
-        .required()
-    };
-    return joi.validate(body, schema);
-  }
-
-
-function getAllEntries(req,res){
-res.status(200).json(entries);      
+  const schema = {
+    title: joi
+      .string()
+      .min(1)
+      .required(),
+    description: joi
+      .string()
+      .min(1)
+      .required()
+  };
+  return joi.validate(body, schema);
 }
 
-
-function getOneEntry(req,res){
-    const entryId = req.params.id;
-    const entry = entries.find(val => val.id === parseInt(entryId, 10));
-    if (!entry) return res.status(404).send('The entry with this id does not exist');
-    return res.status(200).json(entry);   
+function getAllEntries(req, res) {
+  res.status(200).json(entries);
 }
 
-function addEntry(req,res){
-const { error } = validateEntry(req.body);
+function getOneEntry(req, res) {
+  const entryId = req.params.id;
+  const entry = entries.find(val => val.id === parseInt(entryId, 10));
+  if (!entry) return res.status(404).send('The entry with this id does not exist');
+  return res.status(200).json(entry);
+}
+
+function addEntry(req, res) {
+  const { error } = validateEntry(req.body);
   if (error) return res.status(400).send(error.details[0].message);
   let newId;
   if (entries.length > 0) {
@@ -48,8 +46,8 @@ const { error } = validateEntry(req.body);
   return res.status(201).json(entry);
 }
 
-function updateEntry(req,res){
-    const entryId = req.params.id;
+function updateEntry(req, res) {
+  const entryId = req.params.id;
 
   const entry = entries.find(val => val.id === parseInt(entryId, 10));
   if (!entry) return res.status(404).send('No diary entry with the give id');
@@ -65,10 +63,10 @@ function updateEntry(req,res){
   entry.description = updatedEntry.description;
 
   return res.status(200).json(entry);
-  
 }
-function deleteOneEntry(req,res){
-    const entryId = req.params.id;
+
+function deleteOneEntry(req, res) {
+  const entryId = req.params.id;
 
   const entry = entries.find(val => val.id === parseInt(entryId, 10));
   if (!entry) return res.status(404).send('No diary entry with the given id');
@@ -76,20 +74,19 @@ function deleteOneEntry(req,res){
   const entryIndex = entries.indexOf(entry);
   entries.splice(entryIndex, 1);
 
-  return res.status(200).json(entry);  
+  return res.status(200).json(entry);
 }
-function deleteAllEntries(req,res){
-    entries.splice(0, entries.length);
 
-  return res.status(200).json(entries);  
+function deleteAllEntries(req, res) {
+  entries.splice(0, entries.length);
+
+  return res.status(200).json(entries);
 }
-module.exports={
-    getAllEntries,
-    getOneEntry,
-    addEntry,
-    updateEntry,
-    deleteOneEntry,
-    deleteAllEntries
-
-
-}
+module.exports = {
+  getAllEntries,
+  getOneEntry,
+  addEntry,
+  updateEntry,
+  deleteOneEntry,
+  deleteAllEntries
+};
