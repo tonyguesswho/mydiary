2e1ea3de21c487e6124cb433fe620af19bf97385
Anthony ugwu <anthonyu234@gmail.com>
2018-07-27 08:19:14 -0700

    Adjust table

--- database/entry.sql
+++ database/entry.sql
@@ -7,7 +7,7 @@ CREATE TABLE entries (
   ID SERIAL PRIMARY KEY,
   title TEXT,
   description  VARCHAR,
-  usrid int FOREIGN KEY,
+  userid INTEGER
   created_at timestamp with time zone NOT NULL DEFAULT now(),
   updated_at timestamp with time zone NOT NULL DEFAULT now()
   
