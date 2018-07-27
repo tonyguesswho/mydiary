c4b18f6fc28668b1e6b752a048e6b4fe4a6c03b3
Anthony ugwu <anthonyu234@gmail.com>
2018-07-27 12:02:05 -0700

    Rectify tests

--- database/entry.sql
+++ database/entry.sql
@@ -7,13 +7,9 @@ CREATE TABLE entries (
   ID SERIAL PRIMARY KEY,
   title TEXT,
   description  VARCHAR,
-<<<<<<< HEAD
   userid INTEGER
   created_at timestamp with time zone NOT NULL DEFAULT now(),
   updated_at timestamp with time zone NOT NULL DEFAULT now()
-=======
-  userId int
->>>>>>> 0c1099610de85d43f362acd1ff1f48e9782aea9c
   
 );
 
