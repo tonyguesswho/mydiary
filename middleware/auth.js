1cc8e6946d749d36c2ad4b26835231ccde2d33b0
Anthony ugwu <anthonyu234@gmail.com>
2018-07-28 15:26:53 -0700

    Setup travis

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,15 +1,15 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const checkAuth = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(' ')[1];
+    const token = req.headers.authorization.split(" ")[1];
 
     const decoded = jwt.verify(token, process.env.JWT_KEY);
     req.userData = decoded;
     next();
   } catch (error) {
     res.status(401).json({
-      message: 'Auth failed'
+      message: "Auth failed"
     });
   }
 };
