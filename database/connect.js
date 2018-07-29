49f2dc47b176f0bfa2238ee400d0a8f6ed431603
Anthony ugwu <anthonyu234@gmail.com>
2018-07-28 17:50:04 -0700

    Setup test

--- database/connect.js
+++ database/connect.js
@@ -2,7 +2,7 @@ const pgp = require('pg-promise')();
 
 require('dotenv').config();
 
-const db = pgp(process.env.DATABASE_URL);
+const db = pgp('postgres://postgres:guesswho@localhost:5432/mydiaryapi');
 
 module.exports = {
   db
