da6fded65bc784ca6766a42c1d70906bf081b36c
Anthony ugwu <anthonyu234@gmail.com>
2018-07-29 22:28:42 -0700

    Mergerd test branch

--- controllers/entries.js
+++ controllers/entries.js
@@ -1,97 +1,97 @@
-import query from "../database/query";
+// import query from "../database/query";
 
-import validator from "../validation/validate";
+// import validator from "../validation/validate";
 
-function getAllEntries(req, res) {
-  const userid = req.userData.userId;
-  query
-    .getAllEntries(userid)
-    .then(data => {
-      res.status(200).json({
-        status: "success",
-        data
-      });
-    })
-    .catch(err => {
-      res.json(err);
-    });
-}
+// function getAllEntries(req, res) {
+//   const userid = req.userData.userId;
+//   query
+//     .getAllEntries(userid)
+//     .then(data => {
+//       res.status(200).json({
+//         status: "success",
+//         data
+//       });
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// }
 
-function getOneEntry(req, res) {
-  const { id } = req.params;
-  const userid = req.userData.userId;
-  query
-    .getOneEntry(userid, id)
-    .then(data => {
-      res.status(200).json({
-        status: "success",
-        data
-      });
-    })
-    .catch(err => {
-      res.json(err);
-    });
-}
+// function getOneEntry(req, res) {
+//   const { id } = req.params;
+//   const userid = req.userData.userId;
+//   query
+//     .getOneEntry(userid, id)
+//     .then(data => {
+//       res.status(200).json({
+//         status: "success",
+//         data
+//       });
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// }
 
-function addEntry(req, res) {
-  const { error } = validator.validateEntry(req.body);
-  if (error) res.status(400).json({ message: error.details[0].message });
-  const id = req.userData.userId;
-  query
-    .addOneEntry(req.body.title, req.body.description, id)
-    .then(data => {
-      res.status(201).json({
-        status: "success",
-        message: "Entry Created succesfully",
-        data
-      });
-    })
-    .catch(err => {
-      res.json(err);
-    });
-}
+// function addEntry(req, res) {
+//   const { error } = validator.validateEntry(req.body);
+//   if (error) res.status(400).json({ message: error.details[0].message });
+//   const id = req.userData.userId;
+//   query
+//     .addOneEntry(req.body.title, req.body.description, id)
+//     .then(data => {
+//       res.status(201).json({
+//         status: "success",
+//         message: "Entry Created succesfully",
+//         data
+//       });
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// }
 
-function updateEntry(req, res) {
-  const { error } = validator.validateEntry(req.body);
-  if (error) res.status(400).send(error.details[0].message);
-  const { title, description } = req.body;
-  const userid = req.userData.userId;
+// function updateEntry(req, res) {
+//   const { error } = validator.validateEntry(req.body);
+//   if (error) res.status(400).send(error.details[0].message);
+//   const { title, description } = req.body;
+//   const userid = req.userData.userId;
 
-  query
-    .updateOneEntry(parseInt(req.params.id, 10), title, description, userid)
-    .then(data => {
-      res.status(200).json({
-        status: "success",
-        message: "Entry updated succesfully",
-        data
-      });
-    })
-    .catch(err => {
-      res.json(err);
-    });
-}
+//   query
+//     .updateOneEntry(parseInt(req.params.id, 10), title, description, userid)
+//     .then(data => {
+//       res.status(200).json({
+//         status: "success",
+//         message: "Entry updated succesfully",
+//         data
+//       });
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// }
 
-function deleteOneEntry(req, res) {
-  const { id } = req.params;
-  const userid = req.userData.userId;
-  query
-    .deleteEntry(id, userid)
-    .then(data => {
-      res.status(200).json({
-        status: "success",
-        message: "Entry deleted succesfully",
-        data
-      });
-    })
-    .catch(err => {
-      res.json(err);
-    });
-}
+// function deleteOneEntry(req, res) {
+//   const { id } = req.params;
+//   const userid = req.userData.userId;
+//   query
+//     .deleteEntry(id, userid)
+//     .then(data => {
+//       res.status(200).json({
+//         status: "success",
+//         message: "Entry deleted succesfully",
+//         data
+//       });
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// }
 
-export default {
-  getAllEntries,
-  getOneEntry,
-  addEntry,
-  updateEntry,
-  deleteOneEntry
-};
+// export default {
+//   getAllEntries,
+//   getOneEntry,
+//   addEntry,
+//   updateEntry,
+//   deleteOneEntry
+// };
