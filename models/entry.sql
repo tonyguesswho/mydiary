be0162787e8286ab1564074f9ed5582615745e55
Anthony ugwu <anthonyu234@gmail.com>
2018-08-01 01:03:40 -0700

    Heroku server

--- models/entry.sql
+++ models/entry.sql
@@ -2,7 +2,7 @@ CREATE TABLE entries (
   ID SERIAL PRIMARY KEY,
   title TEXT,
   description  VARCHAR,
-  userid INTEGER
+  userid INTEGER,
   created_at timestamp with time zone NOT NULL DEFAULT now(),
   updated_at timestamp with time zone NOT NULL DEFAULT now()
   
