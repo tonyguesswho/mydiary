d72d6f5c39043ebe6797465afa18edf1e4b1ec6b
Anthony ugwu <anthonyu234@gmail.com>
2018-08-01 22:56:29 -0700

    Configure swagger

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
 import entries from "../routes/entries";
 import home from "../routes/home";
 import user from "../routes/user";
@@ -10,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/api/v1/entries", entries);
 app.use("/", home);
