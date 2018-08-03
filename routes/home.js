3680cd56f1bfa4a114bb76007a1c4d96e4a46335
Anthony ugwu <anthonyu234@gmail.com>
2018-08-03 02:04:43 +0100

    Adjust homepage message

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,8 @@
-import { Router } from 'express';
+import { Router } from "express";
 
 const router = Router();
-router.get('', (req, res) => {
-  res.json({ message: 'Mydiary API' });
+router.get("", (req, res) => {
+  res.json({ message: "Mydiary API" });
 });
 
 export default router;
