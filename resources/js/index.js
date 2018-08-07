4c1bac51b3724d2641d5ec8ff3215e238b889d54
Anthony ugwu <anthonyu234@gmail.com>
2018-08-07 02:37:08 +0100

    Consume API

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,5 +1,5 @@
 const token=localStorage.getItem('token')
-if(token){
+if(typeof(token) !== "undefined"){
     document.getElementById('authNav').innerHTML='<li><a href="profile.html" id="">Profile</a></li><li><a href="#" id="logout">Sign Out</a></li>'
 }
 document.getElementById('myDiary').addEventListener('click',checkToken)
