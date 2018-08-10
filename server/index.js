3f6f7eed21e27894410aab795ab0ec955159247d
Anthony ugwu <anthonyu234@gmail.com>
2018-08-10 10:29:09 +0100

    Document API

--- server/index.js
+++ server/index.js
@@ -13,15 +13,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
-const options = {
-  customCss: ".swagger-ui .topbar { display: none }"
-};
-
-app.use("/", serve, setup(swaggerDocument, options));
 
 app.use("/api/v1/entries", entries);
 
 app.use("/auth", user);
+const options = {
+  customCss: ".swagger-ui .topbar { display: none }"
+};
+app.use("/", serve, setup(swaggerDocument, options));
 
 const port = process.env.PORT || 3000;
 
